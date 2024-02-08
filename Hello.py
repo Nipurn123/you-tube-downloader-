b31cad7a280e6e6b14ad33751eefbcf136e9e39e
Nipurn123 <154745876+Nipurn123@users.noreply.github.com>
2024-02-09 00:56:14 +0530

    Hello.py

--- Hello.py
+++ Hello.py
@@ -1,39 +1,52 @@
-import os
 import streamlit as st
 from pytube import YouTube, Playlist
+import os
 
 class YouTubeDownloaderGUI:
     def __init__(self):
         st.title("YouTube Downloader")
 
         self.url = st.text_input("Enter YouTube URL:")
-        self.path = st.text_input("Select Download Path:", value=os.path.expanduser("~/Downloads"))
-
         self.download_button = st.button("Download")
+
+        self.path = None
+        self.download_location_button = st.button("Choose Download Location")
+        if self.download_location_button:
+            self.choose_download_location()
 
         if self.download_button:
             self.download()
 
+    def choose_download_location(self):
+        uploaded_file = st.file_uploader("Choose download location", type="")
+
+        if uploaded_file is not None:
+            self.path = os.path.dirname(uploaded_file.name)
+            st.write("Download location chosen:", self.path)
+
     def download(self):
         url = self.url
-        download_path = self.path
 
-        if not url or not download_path:
-            st.error("Please enter a valid URL and download path.")
+        if not url:
+            st.error("Please enter a valid URL.")
+            return
+
+        if self.path is None:
+            st.error("Please choose a download location.")
             return
 
         try:
             if 'playlist' in url:
-                self.download_playlist(url, download_path)
+                self.download_playlist(url)
             else:
-                self.download_single_video(url, download_path)
+                self.download_single_video(url)
 
             st.success("Download completed successfully.")
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-    def download_playlist(self, url, path):
+    def download_playlist(self, url):
         playlist = Playlist(url)
         st.write(f'Downloading playlist: {playlist.title}')
 
@@ -45,10 +58,10 @@ class YouTubeDownloaderGUI:
                 st.warning(f"No suitable stream available for '{yt.title}'. Skipping.")
                 continue
 
-            video_stream.download(path)
+            video_stream.download(self.path)
             st.write(f"Downloaded '{yt.title}' successfully.")
 
-    def download_single_video(self, url, path):
+    def download_single_video(self, url):
         yt = YouTube(url)
         video_stream = yt.streams.get_highest_resolution()
 
@@ -56,7 +69,7 @@ class YouTubeDownloaderGUI:
             st.warning(f"No suitable stream available for '{yt.title}'.")
             return
 
-        video_stream.download(path)
+        video_stream.download(self.path)
         st.write(f"Downloaded '{yt.title}' successfully.")
 
 if __name__ == "__main__":
